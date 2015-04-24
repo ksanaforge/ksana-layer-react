22824fb8f1d16a20976429448442455eee219c85
yapcheahshen <yapcheahshen@gmail.com>
2015-04-24 15:55:53 +0800

    non stop for subjoined consonants and dependent vowels

--- src/caretpos.js
+++ src/caretpos.js
@@ -4,28 +4,35 @@ var create=function(_text) {
 	caretPos.get=function(){
 		return pos;
 	}
-	var isSurrogateHigh=function(code) {
-		return (code>=0xD800 && code<=0xDBFF);
-	}
-	var isSurrogateLow=function(code) {
-		return (code>=0xDC00 && code<=0xDFFF);
+
+	var nonstop=function(code) {
+		return (code>=0xDC00 && code<=0xDFFF) || (code>=0x0f71 && code<=0x0f87)|| (code>=0x0f8d && code<=0x0fbc);
 	}
 	var snapNext=function(_pos) {
 		if (typeof _pos=="undefined") _pos=pos;
 		var code=text.charCodeAt(_pos);
-		if (isSurrogateHigh(code)) _pos++;
+		while (nonstop(code)) {
+			_pos++;
+			code=text.charCodeAt(_pos);
+		}
 		return _pos	}
 	var snapPrev=function(_pos) {
 		if (typeof _pos=="undefined") _pos=pos;
 		var code=text.charCodeAt(_pos);
-		if (isSurrogateLow(code)) _pos--;
+		while (nonstop(code)) {
+			_pos--;
+			code=text.charCodeAt(_pos);
+		}
 		return _pos;	
 	}	
 	caretPos.next=function(_pos){
 		pos=snapPrev(_pos);
 		pos++;	
 		var code=text.charCodeAt(pos);
-		if (isSurrogateLow(code)) pos++;
+		while (nonstop(code)) {
+			pos++;
+			code=text.charCodeAt(pos);
+		}
 		if (pos>text.length) pos=text.length;
 		return pos;
 	}
@@ -33,7 +40,10 @@ var create=function(_text) {
 		pos=snapNext(_pos);
 		pos--;
 		var code=text.charCodeAt(pos);
-		if (isSurrogateLow(code)) pos--;
+		while (nonstop(code)) {
+			pos--;
+			code=text.charCodeAt(pos);
+		}
 		if (pos<0) pos=0;
 		return pos;
 	}
--- test/test_caretpos.js
+++ test/test_caretpos.js
@@ -1,5 +1,6 @@
 var assert=require("assert");
 var caretPos=require('../src/caretpos');
+
 describe("caret Pos for ascii",function(){
 	var text="abcd";
 	var caretpos=caretPos.create(text);
@@ -36,5 +37,30 @@ describe("caret Pos for Chinese",function(){
 	it("prev",function(){
 		assert.equal(caretpos.prev(),3);
 		assert.equal(caretpos.prev(),1);
+	});
+});
+
+
+describe("caret Pos for Tibetan",function(){ //only skip dependent vowels
+	var text="རསྟེབཅོམ་བ";  //valid caret point "|ར|སྟེ|བ|ཅོ|མ|་|བ"
+	var caretpos=caretPos.create(text);
+	it("next",function(){
+		assert.equal(caretpos.next(),1);
+		assert.equal(caretpos.next(),4);
+		assert.equal(caretpos.next(),5);
+		assert.equal(caretpos.next(),7);
+		assert.equal(caretpos.next(),8);
+		assert.equal(caretpos.next(),9);
+		assert.equal(caretpos.next(),10);
+	});
+	it("prev",function(){
+		assert.equal(caretpos.prev(),9);
+		assert.equal(caretpos.prev(),8);
+		assert.equal(caretpos.prev(),7);
+		assert.equal(caretpos.prev(),5);
+		assert.equal(caretpos.prev(),4);
+		assert.equal(caretpos.prev(),1);
+		assert.equal(caretpos.prev(),0);
+		assert.equal(caretpos.prev(),0);
 	});
 });
