256905a7d42038b36eaf6702002e386b51661fdb
yapcheahshen <yapcheahshen@gmail.com>
2015-05-08 20:26:48 +0800

    hover to show revision

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ var FlattenView=require("./src/flattenview");
 var SelectableView=require("./src/selectableview");
 var InterlineView=require("./src/interlineview");
 
-var BaseView=require("./src/baseview");
-var MultiSelectView=require("./src/multiselectview");
-//var ReviseView=require("./src/reviseview");
 var textrange=require("./src/textrange");
-var RevisionView=require("./src/revisionview");
+
+
+//var BaseView=require("./src/baseview");
+//var MultiSelectView=require("./src/multiselectview");
+//var ReviseView=require("./src/reviseview");
+
+
+//var RevisionView=require("./src/revisionview");
 
 module.exports={FlattenView:FlattenView,SelectableView:SelectableView,InterlineView:InterlineView,
-	BaseView:BaseView,MultiSelectView:MultiSelectView,textrange:textrange,RevisionView:RevisionView};
+	textrange:textrange};
--- src/typedef/revision.js
+++ src/typedef/revision.js
@@ -10,22 +10,33 @@ var update=React.addons.update, E=React.createElement, PT=React.PropTypes;
 
 var IL=require("./interline");
 
+
 var AuthorButton=React.createClass({
 	mixins:[PureRenderMixin]
 	,propTypes:{
 		action:PT.func.isRequired
 	}
+	,style:{
+			borderStyle:"solid",borderColor:"gray",fontSize:"50%",color:"silver",borderWidth:1
+			,borderRadius:"20%",cursor:"pointer",verticalAlign:"top",
+			backgroundColor:"drakgray",height:"0.5em",width:"0.5em"
+	}
 	,getInitialState:function() {
 		return {style:{}};
 	}
 	,onMouseLeave:function(e) {
+		this.style.borderColor="gray";
+		this.forceUpdate();
 		this.props.action("leave",this.props.mid);
 	}
 	,onMouseEnter:function(e) {
+		this.style.borderColor="yellow";
+		this.forceUpdate();
 		this.props.action("enter",this.props.mid);
 	}
 	,render:function(){
-		return E("span",{style:this.state.style,onMouseEnter:this.onMouseEnter,onMouseLeave:this.onMouseLeave},
+		return E("span",{style:this.style,
+			onMouseEnter:this.onMouseEnter,onMouseLeave:this.onMouseLeave},
 			this.props.children);
 	}
 });
@@ -58,11 +69,12 @@ var RevisionType=React.createClass({
 	,componentWillUpdate:function(nextProps) {
 		this.style={display:"none"};
 		if (nextProps.hovering) this.style={textDecoration:"underline",display:"inline"};
+		else if (nextProps.activated) this.style={display:"inline"};
 	}
 	,render:function() {
 		var action=this.props.context.action;
 		var author=E(AuthorButton,{action:action,mid:this.props.mid},this.props.markup.author);
-		var note=E(Note,{action:action,mid:this.props.mid},this.props.markup.note);
+		var note=this.props.hovering?E(Note,{action:action,mid:this.props.mid},this.props.markup.note):null;
 
 		return E(IL.Container,{},
 				 E(IL.Super, {}, author)
